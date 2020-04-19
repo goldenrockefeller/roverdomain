cb96289849ae6967238bd0a8b65d2a2ae8545753
Golden Rockefeller <rockefeg@oregonstate.edu>
2020-04-19 14:21:15 -0700

    Update the init file to compile during import

--- src/rockefeg/roverdomain/__init__.py
+++ src/rockefeg/roverdomain/__init__.py
@@ -1,11 +1,1 @@
-from .rover_domain import RoverDomain
-from .base_evaluator import BaseEvaluator
-from .default_evaluator import DefaultEvaluator
-from .difference_evaluator import DifferenceEvaluator
-from .base_dynamics_processor import BaseDynamicsProcessor
-from .default_dynamics_processor import DefaultDynamicsProcessor
-from .base_rover_observations_calculator import BaseRoverObservationsCalculator
-from .default_rover_observations_calculator \
-    import DefaultRoverObservationsCalculator
-from .state import State
-
+import pyximport; pyximport.install()
