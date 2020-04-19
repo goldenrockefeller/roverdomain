e6be8ba6a5fa34e9231300c14c95155c1c873006
Golden Rockefeller <rockefeg@oregonstate.edu>
2020-04-19 14:20:22 -0700

    Update the setup file to compile during import

--- setup.py
+++ setup.py
@@ -1,33 +1,21 @@
 from setuptools import setup, find_namespace_packages
-from setuptools.extension import Extension
-from Cython.Build import cythonize
-import Cython.Compiler.Options
-import os
-import sys
-import numpy
+
 
 if __name__ == "__main__":
 
     namespace = "rockefeg"
     pkg = "roverdomain"
-    
+
+
     setup(
         name = "{namespace}.{pkg}".format(**locals()),
-        version='0.0.6',
-        ext_modules =  (
-            cythonize(
-                "src/{namespace}/{pkg}/*.pyx".format(**locals()), 
-                force=False,
-                include_path = ["src/"],
-                compiler_directives={
-                    'language_level': 3,},)),
+        version='0.0.0',
+        ext_modules = [],
         zip_safe=False,
         packages=find_namespace_packages("src"),
-        package_dir={'': 'src'}, 
-        package_data={"": ["*.pxd"]},
-        install_requires=['setuptools', 'cython', 'numpy', 'rockefeg.ndarray'],
-        include_dirs=[numpy.get_include()],
-        setup_requires = ['cython', 'numpy', 'rockefeg.ndarray'],
-        script_args = ["build_ext",  "install"],
+        package_dir={'': 'src'},
+        install_requires=['cython', 'numpy'],
+        package_data={"": ["*.pxd", "*.pyx", "*.pyxbld"]},
+        script_args = ["install"],
         namespace_packages = [namespace],
-        python_requires = ">=3.5")
+        python_requires = ">=3.3")
